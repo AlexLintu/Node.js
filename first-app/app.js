9618e684a94006bb80099768dd066d4e186e704b
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-12-14 13:54:35 -0800

    Added modules

--- first-app/app.js
+++ first-app/app.js
@@ -1,5 +1,15 @@
-function sayHello(name) {
-  console.log(`Hello ${name}`);
-}
+// Path: https://nodejs.org/dist/latest-v10.x/docs/api/path.html
+const path = require('path');
 
-sayHello('Alex');
+const pathObj = path.parse(__filename);
+
+console.log(pathObj);
+
+// OS: https://nodejs.org/dist/latest-v10.x/docs/api/os.html
+const os = require('os');
+
+const totalMemory = os.totalmem();
+console.log(`Total Memory: ${totalMemory}`);
+
+const freeMemory = os.freemem();
+console.log(`Free Memory: ${freeMemory}`);
