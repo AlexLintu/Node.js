0dbb7a4c40171c6e672a287f2214d9a4f1e6b6d9
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-12-14 14:14:42 -0800

    Added 2 methods

--- first-app/app.js
+++ first-app/app.js
@@ -13,3 +13,16 @@ console.log(`Total Memory: ${totalMemory}`);
 
 const freeMemory = os.freemem();
 console.log(`Free Memory: ${freeMemory}`);
+
+// FileSystem: https://nodejs.org/dist/latest-v10.x/docs/api/fs.html
+const fs = require('fs');
+
+// Synchronous method:
+const files = fs.readdirSync('./')
+console.log(files);
+
+// Asynchronous method (recommended):
+fs.readdir('./', function (err, files) {
+  if (err) console.log('Error'.err);
+  else console.log('Result', files);
+});
