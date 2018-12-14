237a37b3a6dd3c22f7fb3f88b9ad3b4a98f4c7fe
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-12-14 14:51:41 -0800

    Completed the test app

--- first-app/logger.js
+++ first-app/logger.js
@@ -1,14 +1,15 @@
 const EventEmitter = require('events');
-const emitter = new EventEmitter();
 
 const url = 'http://mylogger.io/log';
 
-function log(message) {
-  // Send an HTTP request
-  console.log(message);
-  // Raise an event:
-  emitter.emit('messageLogged', { id: 1, url: 'http://' });
-
+class Logger extends EventEmitter {
+  log(message) {
+    // Send an HTTP request
+    console.log(message);
+    // Raise an event:
+    this.emit('messageLogged', { id: 1, url: 'http://' });
+  }
 }
 
-module.export = log;
+
+module.exports = Logger;
