f78a6c3f5b8b55c78d6626fabfc775f48c64760e
Alexandra Ogarok <alexandra.ogarok@gmail.com>
2020-06-27 15:23:51 -0700

    Process Object

---
+++ process-object/script.sh
@@ -0,0 +1,7 @@
+node app.js Test
+
+# Your word is Test
+# Starting memory usage: 3807072. 
+# Current memory usage: 4250336. 
+# After using the loop to add elements to the array, the process is using 445464 more bytes of memory.
+# Alexs-MacBook-Pro:process-object alexlintu$ 
