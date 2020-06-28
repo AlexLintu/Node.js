a09017c1b5e89202c72d16b2791439d7060e2976
Alexandra Ogarok <alexandra.ogarok@gmail.com>
2020-06-27 18:52:13 -0700

    Event-Driven Architecture

---
+++ 3-event-driven-architecture/app.js
@@ -0,0 +1,10 @@
+let events = require('events');
+
+let listenerCallback = (data) => {
+  console.log('Celebrate ' + data);
+}
+
+let myEmitter = new events.EventEmitter();
+
+myEmitter.on('celebration', listenerCallback);
+myEmitter.emit('celebration', 'Canada Day');
